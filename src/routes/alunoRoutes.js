896732d5a507ec7d802064f85caeab336959983d
jardeylson jacinto <jardeylsong.m@gmail.com>
2023-09-06 10:32:59 -0300

    fix: a bug fix

--- src/routes/alunoRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import alunoController from '../controllers/AlunoController';
-
-const router = new Router();
-
-router.get('/', alunoController.index);
-
-export default router;
