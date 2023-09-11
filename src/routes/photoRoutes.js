293a13c840c92f7a5d82f906e6a58d00e024b6af
jardeylson jacinto <jardeylsong.m@gmail.com>
2023-09-11 14:37:44 -0300

    feat: processing uploaded files

--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,12 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
-import multerConfig from '../config/multerConfig';
 import photoController from '../controllers/PhotoController';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('photo'), photoController.store);
+router.post('/', photoController.store);
 
 export default router;
