365e0dfb24d8665c522731704645d2162569ccf5
jardeylson jacinto <jardeylsong.m@gmail.com>
2023-09-12 07:31:05 -0300

    feat: save photos in the database

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,12 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
+import Photo from '../models/Photo';
 import Student from '../models/Student';
 import User from '../models/User';
 
-const models = [Student, User];
+const models = [Photo, Student, User];
 
 const connection = new Sequelize(databaseConfig);
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
